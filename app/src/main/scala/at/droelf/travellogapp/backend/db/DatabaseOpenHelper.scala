4ce80f765d31c634e4cf444b3af4a6b01b865e37
Mathias Kub <git@makubi.at>
2014-07-15 07:10:44 +0200

    Problem mit Int --> Constructor geloest

--- app/src/main/scala/at/droelf/travellogapp/backend/db/DatabaseOpenHelper.scala
+++ app/src/main/scala/at/droelf/travellogapp/backend/db/DatabaseOpenHelper.scala
@@ -3,14 +3,14 @@ package at.droelf.travellogapp.backend.db
 import android.database.sqlite.{SQLiteDatabase, SQLiteOpenHelper}
 import at.droelf.travellogapp._
 
-object DatabaseOpenHelper extends SQLiteOpenHelper(AppStatics.context, DatabaseOpenHelper.DATABASE_NAME, null, Integer.parseInt(DatabaseOpenHelper.CURRENT_DATABASE_VERSION)) {
+object DatabaseOpenHelper extends SQLiteOpenHelper(AppStatics.context, DatabaseOpenHelper.DATABASE_NAME, null, DatabaseOpenHelper.CURRENT_DATABASE_VERSION) {
   final val INITIAL_DB_VERSION: Int = 1
   final val CURRENT_DATABASE_VERSION = 2
   final val DATABASE_NAME = "travelLogDb"
   final val QUEUED_IMAGE_UPLOADS_TABLE = "QUEUED_IMAGE_UPLOADS"
 
   def onCreate(db: SQLiteDatabase) {
-    upgradeDb(db, DatabaseOpenHelper.INITIAL_DB_VERSION, Integer.parseInt(DatabaseOpenHelper.CURRENT_DATABASE_VERSION))
+    upgradeDb(db, DatabaseOpenHelper.INITIAL_DB_VERSION, DatabaseOpenHelper.CURRENT_DATABASE_VERSION)
   }
 
   def onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
